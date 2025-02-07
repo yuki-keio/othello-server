b01443d7489819a00e5e01578b49fe08b7f7c380
yuki <yuki-keio@users.noreply.github.com>
2025-02-08 08:46:04 +0900

    2025年 2月 8日 土曜日 08時46分04秒 JST

--- config/settings/__init__.py
+++ config/settings/__init__.py
@@ -2,7 +2,7 @@ import os
 
 DJANGO_SETTINGS_MODULE = os.getenv("DJANGO_SETTINGS_MODULE", "config.settings.local")
 
-if DJANGO_SETTINGS_MODULE == 'local':
+if DJANGO_SETTINGS_MODULE == 'config.settings.local':
     from .local import *
 else:
     from .production import *
