48065e8be5ef99539197db9783cf97b440a65737
yuki <yuki-keio@users.noreply.github.com>
2025-02-11 23:40:25 +0900

    2025-02-11 23:40:25

--- config/urls.py
+++ config/urls.py
@@ -15,7 +15,7 @@ Including another URLconf
 """
 
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
 from django.views.static import serve
 import os
 from django.conf import settings
