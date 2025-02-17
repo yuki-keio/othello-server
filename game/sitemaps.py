41e32b10bb2b6f0f035bd1d0bd825ff783875a4b
yuki <yuki-keio@users.noreply.github.com>
2025-02-17 09:59:48 +0900

    2025-02-17 09:59:48

--- game/sitemaps.py
+++ game/sitemaps.py
@@ -2,16 +2,19 @@ import subprocess
 from django.contrib.sitemaps import Sitemap
 from django.urls import reverse
 from datetime import datetime
+import os
 
-def get_git_lastmod():
-    """最新のGitコミット日を取得する"""
+
+def get_git_lastmod(filepath):
+    """特定のファイルの最新変更日を Git から取得"""
     try:
+        file_path = os.path.join(BASE_DIR, "../", filepath)  # プロジェクトルートからのパス
         last_commit_date = subprocess.check_output(
-            ["git", "log", "-1", "--format=%cd", "--date=iso-strict"]
+            ["git", "log", "-1", "--format=%cd", "--date=iso-strict", "--", file_path]
         ).decode("utf-8").strip()
         return datetime.fromisoformat(last_commit_date)
     except Exception as e:
-        print(f"Error getting last commit date: {e}")
+        print(f"Error getting last commit date for {filepath}: {e}")
         return datetime.utcnow()  # 取得できない場合は現在時刻を返す
 
 class StaticViewSitemap(Sitemap):
