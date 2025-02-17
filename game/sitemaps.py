8645ecbb59caf6caa2369fd0495481822196dd92
yuki <yuki-keio@users.noreply.github.com>
2025-02-17 10:02:32 +0900

    2025-02-17 10:02:32

--- game/sitemaps.py
+++ game/sitemaps.py
@@ -1,9 +1,10 @@
 import subprocess
+import os
 from django.contrib.sitemaps import Sitemap
 from django.urls import reverse
 from datetime import datetime
-import os
 
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 
 def get_git_lastmod(filepath):
     """特定のファイルの最新変更日を Git から取得"""
@@ -21,10 +22,20 @@ class StaticViewSitemap(Sitemap):
     priority = 0.5        
 
     def items(self):
-        return ['player-mode', 'ai-mode', 'online-mode', 'blog-strategy']
+        return [
+            ('player-mode', "templates/game/player.html"),
+            ('ai-mode', "templates/game/ai.html"),
+            ('online-mode', "templates/game/online.html"),
+            ('blog-strategy', "templates/game/strategy-reversi-othello.html"),
+        ]
 
     def location(self, item):
-        return reverse(item)
+        """URL を適切に出力"""
+        if item[0] == 'blog-strategy':
+            return 'https://reversi.yuki-lab.com/strategy-reversi-othello.html'
+        return f'https://reversi.yuki-lab.com{reverse(item[0])}'
 
     def lastmod(self, item):
-        return get_git_lastmod()
+        """最終更新日を ISO 8601 形式 (YYYY-MM-DDTHH:MM:SS+TZ) で返す"""
+        lastmod_date = get_git_lastmod(item[1])
+        return lastmod_date.isoformat()  # Google 用に時間情報も含める
