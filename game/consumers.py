727262ff0ea330d66ade38791915d765e218a72e
yuki <yuki-keio@users.noreply.github.com>
2025-02-20 00:55:18 +0900

    2025-02-20 00:55:18

--- game/consumers.py
+++ game/consumers.py
@@ -33,8 +33,6 @@ class OthelloConsumer(AsyncWebsocketConsumer):
             player_id = query_params.get("playerId", [None])[0]
             player_name = query_params.get("playerName", [None])[0]
             language = query_params.get("lang", ["ja"])[0]
-            print(language)
-            print(qery_params.get("lang"))
             activate(language)
             
            
