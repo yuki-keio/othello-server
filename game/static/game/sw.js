59383bf38b0c2fe3cb5867eb985d34b487d3a96a
yuki <yuki-keio@users.noreply.github.com>
2025-02-26 23:13:01 +0900

    2025-02-26 23:13:01

--- game/static/game/sw.js
+++ game/static/game/sw.js
@@ -3,7 +3,21 @@ const urlsToCache = [
     "/",
     "/static/game/images/favicon/favicon.ico",
     "/static/game/images/favicon/favicon-192x192.png",
-    "/static/game/images/favicon/favicon-512x512.png"
+    "/static/game/images/favicon/favicon-512x512.png",
+    "/static/game/images/favicon/apple-touch-icon.png",
+    "/static/game/script.js",
+    "/static/game/confetti.browser.min.js",
+    "/static/game/images/setting.svg",
+    "/static/game/images/qr.svg",
+    "/static/game/images/copy.webp",
+    "/static/game/sounds/defeat.mp3",
+    "/static/game/sounds/victory.mp3",
+    "/static/game/sounds/place-stone.mp3",
+    "/static/game/sounds/warning.mp3",
+    "/static/game/style.css",
+    "/static/game/strategy.css",
+    "/static/template/game/index.html",
+    "/static/template/game/strategy-reversi-othello.html",
 ];
 
 // インストール時にキャッシュする
