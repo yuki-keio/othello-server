2633a2c1a93ea8f66fff34956b4097e9e2ed6b4d
yuki <yuki-keio@users.noreply.github.com>
2025-04-26 14:35:40 +0900

    2025-04-26 14:35:40

--- config/settings/base.py
+++ config/settings/base.py
@@ -216,7 +216,9 @@ CHANNEL_LAYERS = {
     "default": {
         "BACKEND": "channels_redis.core.RedisChannelLayer",
         "CONFIG": {
-            "hosts": [os.environ.get("REDIS_URL", "redis://127.0.0.1:6379")],
+            "hosts": [
+                os.environ.get("REDIS_URL", "redis://127.0.0.1:6379")
+                ],
         },
     },
 }
