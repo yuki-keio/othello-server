b6f74851ccfc3280ae614ea839e129474543f0e1
yuki <yuki-keio@users.noreply.github.com>
2025-05-01 21:09:16 +0900

    2025-05-01 21:09:16

--- config/settings/production.py
+++ config/settings/production.py
@@ -87,6 +87,7 @@ CSP_STYLE_SRC = (
     "https://pagead2.googlesyndication.com",
     "https://reversi.yuki-lab.com/static/",  
     "http://127.0.0.1:8000/static/",  
+    "https://cdnjs.cloudflare.com",
 )
 CSP_STYLE_SRC_ATTR = ("'unsafe-inline'",) 
 CSP_FONT_SRC = ("'self'", "https://fonts.gstatic.com")
