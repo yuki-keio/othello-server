6b169a02980dfb66a4415708d13e0f47a4858547
yuki <yuki-keio@users.noreply.github.com>
2025-05-04 09:26:32 +0900

    2025-05-04 09:26:32

--- config/settings/production.py
+++ config/settings/production.py
@@ -46,6 +46,7 @@ CSP_CONNECT_SRC = (
 
 CSP_SCRIPT_SRC = (
     "'self'",
+    "'wasm-unsafe-eval'",
     "https://www.google.com",
     "https://www.gstatic.com",
     "https://ep2.adtrafficquality.google",
